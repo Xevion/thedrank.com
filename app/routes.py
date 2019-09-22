b3f633e994570202833500d37df41f57325c5248
Xevion <xevioni@yandex.com>
2019-09-22 15:10:43 -0500

    viewcounter addition

--- app/routes.py
+++ app/routes.py
@@ -3,9 +3,15 @@ from flask import render_template
 import os
 import sys
 
+i = int(open(os.path.join('app', 'static', 'viewcount.dat'), 'r').read())
+def getIncrement(n=1):
+    i += 1
+    open(os.path.join, 'w').write(str(i))
+    return i
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', viewcount=getIncrement())
 
 @app.route('/keybase.txt')
 @app.route('/.well-known/keybase.txt')
