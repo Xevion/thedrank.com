36b22c33154d8ddb422c7eb3f67e66888691fb16
Xevion <xevioni@yandex.com>
2019-09-22 14:51:55 -0500

    keybase verification

--- app/routes.py
+++ app/routes.py
@@ -6,3 +6,8 @@ import sys
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/keybase.txt')
+@app.route('/.well-known/keybase.txt')
+def keybase():
+    return app.send_static_file('keybase.txt')
